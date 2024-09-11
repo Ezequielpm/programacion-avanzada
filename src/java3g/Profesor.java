5c63b72d99534e72aacc242039620d866ff8d7fd
Ezequiel <ezequielmazariegossolis@gmail.com>
2024-09-11 08:50:07 -0500

    Se agregan los atributos del profesor asi como también sus getters y setters. (En clase de Carlos Manuel)

--- src/java3g/Profesor.java
+++ src/java3g/Profesor.java
@@ -9,5 +9,52 @@ package java3g;
  * @author ezequ
  */
 public class Profesor {
+
+    private String numPlaza;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private int edad;
+    private String nombre;
+
+    public String getNumPlaza() {
+        return numPlaza;
+    }
+
+    public void setNumPlaza(String numPlaza) {
+        this.numPlaza = numPlaza;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
